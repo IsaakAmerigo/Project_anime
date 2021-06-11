67467ab2135a24288d2c4240d74080a477623d95
isaakamerigo <landen_g@yahoo.com>
2021-06-11 09:41:39 -0400

    added to js, need to XMLHttpRequest block

--- index.js
+++ index.js
@@ -1,15 +1,48 @@
+const dropdown = document.querySelector('#dropdown');
+const imageMain = document.querySelector('#image-container');
 
+// const fetchImage = async (event) => {
+//   imageMain.childNodes.forEach((child) => child.remove());
 
-// const animeURL = "https://api.jikan.moe/v3/search/anime?q=vampire";
-const anime = async(animeURL) => {
-  try {
-const response = await axios.get(animeURL)
-console.log(response);
-  } catch(error) {
-    console.error(error)
+//   const currentDoomer = event.target.value;
+//     const response = await axios.get("https://cdn.myanimelist.net/images/anime//${currentDoomer}/image_url");
+//     const doomerImage = document.createElement('img');
+//     const imageURL = response.data.results;
+//     doomerImage.src = imageURL;
+
+//     imageMain.append(doomerImage);
+//   }
+  
+  
+  
+  const appendWeeb = (doomerObj) => {
+    // const doomers = Object(doomerObj);
+   console.log(doomerObj);
+    doomerObj.forEach((doomer) => {
+      const doomerOption = document.createElement('option');
+      doomerOption.innerText = doomer.title;
+  
+      dropdown.append(doomerOption);
+    })
   }
+  
+  const animeURL =  "https://api.jikan.moe/v3/search/anime?q=vampire"
 
+const anime = async() => {
+  try {
+    const response = await axios.get(animeURL)
+    const doomerObj = response.data.results;
+    console.log(doomerObj)
+
+    appendWeeb(doomerObj);
+    
+    
+    
+  } catch(error) {
+    console.error(error);
+  }
+  
 }
-anime(animeURL);
+// dropdown.addEventListener('change', fetchImage);
 
-let doomer = 
+anime();
